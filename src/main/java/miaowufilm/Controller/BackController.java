0f6d8c872ed764d1fb3c654cb1bd412271bbf676
XXSWKL <1799829206@qq.com>
2024-01-10 03:07:44 +0800

    pom.xml中<groupId>com.baomidou</groupId>的原来版本为3.5.3.1，我的提交修改为3.5.5。 姿：“后台”放在页面top栏；完成POI打印报表、Echarts生成扇形图+柱状图（页面样式待调整）；application需要一句“mybatis-plus.configuration.map-underscore-to-camel-case=false”（未上传）

---
+++ src/main/java/miaowufilm/Controller/BackController.java
@@ -0,0 +1,97 @@
+package miaowufilm.Controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import miaowufilm.entity.Film;
+import miaowufilm.service.FilmService;
+import org.apache.poi.hssf.usermodel.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class BackController {
+    @Autowired
+    private FilmService filmService;
+
+    @RequestMapping("/index/toBackstage")
+    public String toBackstage(){
+        return "backstage";
+    }
+
+    @RequestMapping("/ExcelDownload")
+    public void excelDownload(HttpServletResponse response) throws IOException {
+        //表头数据
+        String[] header={"filmId","filmname","play_number"};
+        HSSFWorkbook workbook=new HSSFWorkbook();
+        //生成一个表格“销售榜单”
+        HSSFSheet sheet=workbook.createSheet("销售榜单");
+        //设置表格列宽度为10个字节
+        sheet.setDefaultColumnWidth(10);
+        //创建第一行表头
+        HSSFRow headrow = sheet.createRow(0);
+
+        //遍历表头
+        for (int i=0;i< header.length;i++){
+            //创建一个单元格
+            HSSFCell cell = headrow.createCell(i);
+            //创建一个内容对象
+            HSSFRichTextString text = new HSSFRichTextString(header[i]);
+            //将内容对象的文字内容写入到单元格中
+            cell.setCellValue(text);
+        }
+
+        //从数据库拿值出来放到表格中
+        List<Film> films = filmService.findAll();
+        for (int i = 0; i < films.size(); i++) {
+            HSSFRow row = sheet.createRow(i + 1);
+            Film film = films.get(i);
+            HSSFCell cell = null;
+            String[] names = {Math.toIntExact(film.getFilmId()) + "",
+                    film.getFilmname()+ "",
+                    film.getPlay_number()+ "",};
+            for (int k = 0; k < names.length; k++) {
+                cell = row.createCell(k);
+                //拿到属性值
+                HSSFRichTextString text = new HSSFRichTextString(names[k]);
+                cell.setCellValue(text);
+            }
+        }
+
+        //准备将Excel的输出流通过response输出到页面下载
+        //八进制输出流
+        response.setContentType("application/octet-stream");
+
+        //设导出Excel的名称为Sales.xls
+        response.setHeader("Content-disposition", "attachment;filename=Sales.xls");
+
+        //刷新缓冲
+        response.flushBuffer();
+
+        //workbook将Excel写入到response的输出流中，供页面下载
+        workbook.write(response.getOutputStream());
+
+        //关闭workbook
+        workbook.close();
+    }
+
+
+    @RequestMapping("/Echarts")
+    @ResponseBody
+    public List<Film> Echarts(Model model){
+        List<Film> films=filmService.findAll();
+        return films;
+    }
+    @RequestMapping("/view")
+    public String goview(){
+        return "POI1";
+    }
+    @RequestMapping("/view1")
+    public String goview1(){
+        return "poi";
+    }
+}
